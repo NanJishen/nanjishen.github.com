93d3bd0f8c17ff728e0560ce60f62af87bebe314
NanJishen <nanji_shen@qq.com>
2021-03-19 21:05:42 +0800

    3月19日

---
+++ minitools/notepad/app.js
@@ -0,0 +1,3 @@
+if(window.localStorage){var d=document,h=location.hash,name='note'+h+'.text',input=d.getElementsByTagName('textarea')[0],text=localStorage[name];if(text){input.value=text;}
+if(h.length>1){d.title='Note '+h;}
+input.addEventListener('input',function(){try{localStorage.setItem(name,input.value);}catch(e){}});}
